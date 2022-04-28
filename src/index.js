648af539acc9a156d8814c27f79cc9819ebc84ef
Jorge Ibarra <hello@jorgeibarra.com.mx>
2022-04-28 17:50:23 -0500

    added src folder with starting files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Starting');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<div>Home</div>);
